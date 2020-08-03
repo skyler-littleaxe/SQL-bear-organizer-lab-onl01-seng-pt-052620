4992f026450d996e7ceae75b821c924e6723cd8a
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-08-02 23:43:32 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 3, "M", "brown", "real af", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Rowdy", 3, "M", "black", "party animal", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Tabitha", 8, "F", "brown", "ratchet", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Sergeamt Brown", 2, "MALE", "not brown", "vibin", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Melissa", 2, "F", "sparkle", "mild", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Grinch", 5, "F", "black", "loving", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Wendy", 4, "F", "brown", "gossipy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES (NULL, 2, "M", "brown", "dead", false);
